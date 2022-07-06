95f6614d17db46e85f4f41cf4ef9ae62c4010ec7
Ihosvani <ihosvani2014@gmail.com>
2022-07-06 13:50:33 -0400

    Added nestjs, prisma and a postgresql database containing an User and a Task and the create-task page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { FooterComponent } from './footer/footer.component';
+import { CreateTaskComponent } from './create-task/create-task.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationBarComponent,
-    FooterComponent
+    FooterComponent,
+    CreateTaskComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
